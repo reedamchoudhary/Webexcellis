ee9b36399bde62a9c4f35c7e299040d126a05278
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2022-05-04 16:02:17 +0530

    initialisation

---
+++ webexcellis/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import EnterBasicInfo from "./Pages/EnterBasicInfo";
+// import "./css/style.css";
+import "./css/bootstrap.min.css";
+import "./css/bootstrap.min.css.map";
+
+function App() {
+  return (
+    <div className="App">
+      <EnterBasicInfo />
+    </div>
+  );
+}
+
+export default App;
